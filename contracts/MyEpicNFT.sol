9a6528bb35b1958a47727c9e414d7137602453b3
Sourabh Agrawal <agrawal.sourabh644@gmail.com>
2021-12-15 00:10:43 +0530

    updated NFT json

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -20,12 +20,10 @@ contract MyEpicNFT is ERC721URIStorage {
 
         _safeMint(msg.sender, newItemId);
 
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/TESK");
+        _setTokenURI(newItemId, "data:application/json;base64,ewogICAgIm5hbWUiOiAiTXlUZXh0TkZUIiwKICAgICJkZXNjcmlwdGlvbiI6ICJNeVRleHRORlQgaXMgYSBzeXN0ZW0gZ2VuZXJhdGVkIHRleHQgaW1hZ2UiLAogICAgImltYWdlIjogImRhdGE6aW1hZ2Uvc3ZnK3htbDtiYXNlNjQsUEhOMlp5QjRiV3h1Y3owaWFIUjBjRG92TDNkM2R5NTNNeTV2Y21jdk1qQXdNQzl6ZG1jaUlIQnlaWE5sY25abFFYTndaV04wVW1GMGFXODlJbmhOYVc1WlRXbHVJRzFsWlhRaUlIWnBaWGRDYjNnOUlqQWdNQ0F6TlRBZ016VXdJajRLSUNBZ0lEeHpkSGxzWlQ0dVltRnpaU0I3SUdacGJHdzZJSGRvYVhSbE95Qm1iMjUwTFdaaGJXbHNlVG9nYzJWeWFXWTdJR1p2Ym5RdGMybDZaVG9nTVRSd2VEc2dmVHd2YzNSNWJHVStDaUFnSUNBOGNtVmpkQ0IzYVdSMGFEMGlNVEF3SlNJZ2FHVnBaMmgwUFNJeE1EQWxJaUJtYVd4c1BTSmliR0ZqYXlJZ0x6NEtJQ0FnSUR4MFpYaDBJSGc5SWpVd0pTSWdlVDBpTlRBbElpQmpiR0Z6Y3owaVltRnpaU0lnWkc5dGFXNWhiblF0WW1GelpXeHBibVU5SW0xcFpHUnNaU0lnZEdWNGRDMWhibU5vYjNJOUltMXBaR1JzWlNJK1RYbFVaWGgwVGtaVVBDOTBaWGgwUGdvOEwzTjJaejQ9Igp9");
 
         console.log("An NFT w/ID %s has been minted to %s", newItemId, msg.sender);
 
         _tokenIDs.increment();
-
-
     }
 }
